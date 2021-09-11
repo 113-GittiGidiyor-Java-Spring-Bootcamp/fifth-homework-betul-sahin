eeec4087e019a213483469effa5f57cc545432c3
Betül Şahin <betulsahin3387@hotmail.com>
2021-09-11 13:50:02 +0300

    created spring boot project and dependencies

---
+++ SchoolManagementSystemV5/src/main/java/com/betulsahin/schoolmanagementsystemv5/SchoolManagementSystemV5Application.java
@@ -0,0 +1,13 @@
+package com.betulsahin.schoolmanagementsystemv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemV5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementSystemV5Application.class, args);
+    }
+
+}
