843e3b5b93541282aad11b7e29eed2427675691f
Betül Şahin <betulsahin3387@hotmail.com>
2021-09-20 07:39:11 +0300

    Fix tests
    Update tests of create method
    Add tests of findAll and findById methods

--- SchoolManagementSystemV5/src/main/java/com/betulsahin/schoolmanagementsystemv5/utils/ErrorMessageConstants.java
+++ SchoolManagementSystemV5/src/main/java/com/betulsahin/schoolmanagementsystemv5/utils/ErrorMessageConstants.java
@@ -2,7 +2,7 @@ package com.betulsahin.schoolmanagementsystemv5.utils;
 
 public class ErrorMessageConstants {
     public static final String NOT_APPROPRIATE_AGE_RANGE = "This age (%s) is not appropriate age range.";
-    public static final String INSTRUCTOR_FOUND = "The instructor is already exist with this phone number %d";
+    public static final String INSTRUCTOR_FOUND = "The instructor is already exist with this phone number %s";
     public static final String COURSE_FOUND = "The course is already exist with this code %s";
     public static final String COURSE_EXCEEDED = "Student count (%d) for one course exceeded exception";
     public static final String STUDENT_NOT_FOUND = "Student not found with this id : %d";
--- SchoolManagementSystemV5/src/test/java/com/betulsahin/schoolmanagementsystemv5/services/InstructorServiceTest.java
+++ SchoolManagementSystemV5/src/test/java/com/betulsahin/schoolmanagementsystemv5/services/InstructorServiceTest.java
@@ -2,17 +2,22 @@ package com.betulsahin.schoolmanagementsystemv5.services;
 
 import com.betulsahin.schoolmanagementsystemv5.dtos.InstructorDto;
 import com.betulsahin.schoolmanagementsystemv5.dtos.PermanentInstructorDto;
+import com.betulsahin.schoolmanagementsystemv5.exceptions.InstructorIsAlreadyExistException;
 import com.betulsahin.schoolmanagementsystemv5.mappers.InstructorMapper;
 import com.betulsahin.schoolmanagementsystemv5.models.PermanentInstructor;
 import com.betulsahin.schoolmanagementsystemv5.models.abstracts.Instructor;
 import com.betulsahin.schoolmanagementsystemv5.repositories.InstructorRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +25,7 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class InstructorServiceTest {
+
     @Mock
     private InstructorRepository mockInstructorRepository;
 
@@ -30,21 +36,63 @@ class InstructorServiceTest {
     private InstructorService underTest;
 
     @Test
+    void itShouldFindAllInstructor(){
+        // given
+        Instructor instructor = new PermanentInstructor();
+        instructor.setName("Ahmet Yılmaz");
+        List<Instructor> instructors = Collections.singletonList(instructor);
+
+        InstructorDto request = new InstructorDto();
+        request.setName(instructor.getName());
+
+        Mockito.when(mockInstructorMapper.mapToDto(any())).
+                thenReturn(request);
+
+        Mockito.when(mockInstructorRepository.findAll()).
+                thenReturn(instructors);
+
+        // when
+        List<InstructorDto> actual = underTest.findAll();
+
+        // then
+        assertEquals(actual.get(0).getName(), "Ahmet Yılmaz");
+    }
+
+    @Test
+    void itShouldReturnInstructor_whenInstructorIdExist(){
+        // given
+        Instructor instructor = new PermanentInstructor();
+
+        InstructorDto request = new InstructorDto();
+
+        Mockito.when(mockInstructorMapper.mapToDto(any())).
+                thenReturn(request);
+
+        Mockito.when(mockInstructorRepository.findById(1L)).
+                thenReturn(Optional.of(instructor));
+
+        // when
+        InstructorDto actual = underTest.findById(1L);
+
+        // then
+        assertEquals(actual, request);
+    }
+
+    @Test
     void itShouldCreateNewInstructor(){
         // given
         Instructor instructor = new PermanentInstructor();
         instructor.setPhoneNumber("5554443322");
 
+        InstructorDto request = new InstructorDto();
+        request.setPhoneNumber(instructor.getPhoneNumber());
+
         Mockito.when(mockInstructorRepository.findByPhoneNumber(any())).
                 thenReturn(Optional.empty());
-
-        Mockito.when(mockInstructorMapper.map(any())).
-                thenReturn(instructor);
 
         Mockito.when(mockInstructorRepository.save(any())).thenReturn(instructor);
 
         // when
-        InstructorDto request = new PermanentInstructorDto();
         Instructor actual = underTest.create(request).get();
 
         // then
@@ -53,5 +101,24 @@ class InstructorServiceTest {
                 () -> assertEquals(instructor, actual),
                 () -> assertEquals(instructor.getPhoneNumber(), actual.getPhoneNumber())
         );
+    }
+
+    @Test
+    void itShouldNotCreateInstructor_whenInstructorExist(){
+        // given
+        Instructor instructor = new PermanentInstructor();
+        instructor.setPhoneNumber("5554443322");
+
+        InstructorDto request = new InstructorDto();
+        request.setPhoneNumber(instructor.getPhoneNumber());
+
+        Mockito.when(mockInstructorRepository.findByPhoneNumber(instructor.getPhoneNumber())).
+                thenReturn(Optional.of(instructor));
+
+        // when
+        Executable executable = () -> this.underTest.create(request).get();
+
+        // then
+        assertThrows(InstructorIsAlreadyExistException.class, executable);
     }
 }
